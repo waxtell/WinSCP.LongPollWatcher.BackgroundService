cff7a7624f5a9bd961d27ae179b6330b5fbb8ede
Will Axtell <waxtell@transtar1.com>
2019-05-21 16:06:28 -0400

    Initial commit

---
+++ WinSCP.LongPollWatcher.BackgroundService/Models/External/LongPollWatcherOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace WinSCP.LongPollWatcher.BackgroundService.Models.External
+{
+    public class LongPollWatcherOptions
+    {
+        public int SleepIntervalMilliseconds { get; set; } = 10000;
+        public string RemotePath { get; set; } = "/";
+        public string Mask { get; set; } = "*.*";
+        public bool IncludeSubdirectories { get; set; } = true;
+        public string SessionExecutablePath { get; set; }
+    }
+}
